f27d4e3deb4baffda50bc25eeffd487aad6d9583
Guilherme <santos.guilherme.dev@gmail.com>
2024-06-04 21:06:27 -0300

    Configurando rota de login

--- src/controller/loginController.js
+++ src/controller/loginController.js
@@ -1,7 +1,9 @@
 import { salvarLogin, listarLogins, buscarUsuario } from "../repository/loginRepository.js";
 import { Router } from "express";
+import jwt from "jsonwebtoken";
 
 const servidor = Router();
+const JWT_SECRET = 'teste';
 
 servidor.post('/login', async (req, resp) => {
     let login = req.body;
@@ -24,10 +26,13 @@ servidor.get('/login', async (req, resp) => {
 });
 
 servidor.post('/login/entrar', async (req, resp) => {
-    let login = req.body;
-    let loginBuscado = await buscarUsuario(login);
+    const login = req.body;
+    const loginBuscado = await buscarUsuario(login);
+    
     if (loginBuscado) {
-        resp.send(loginBuscado);
+        const token = jwt.sign({ id: loginBuscado.id }, JWT_SECRET, { expiresIn: '1h' });
+        
+        resp.send({ login: loginBuscado, token });
     } else {
         resp.status(401).send({ error: 'Usuário ou senha inválidos' });
     }
